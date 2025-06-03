5356f298fce09d709d4509ffbbe090bcbc7b8971
Labh Bothra <labhbothra1380@gmail.com>
2025-06-03 20:29:11 +0530

    Do

--- project-bolt-sb1-7ft7cjbm/project/tailwind.config.js
+++ project-bolt-sb1-7ft7cjbm/project/tailwind.config.js
@@ -1,6 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  safelist: [
+    'h-[500px]',
+    'w-[500px]',
+    'scale-100',
+    'scale-75',
+    'z-10',
+    'z-20',
+    'opacity-60',
+    'border-white',
+  ],
   theme: {
     extend: {},
   },
