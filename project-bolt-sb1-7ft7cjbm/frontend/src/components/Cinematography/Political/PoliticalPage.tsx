16af8cb5977363fb2e233f0bd6d961bd6adbceda
Labh Bothra <labhbothra1380@gmail.com>
2025-06-07 14:55:00 +0530

    DOne

---
+++ project-bolt-sb1-7ft7cjbm/frontend/src/components/Cinematography/Political/PoliticalPage.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Navbar from "../../HomePage/Navbar";
+import EventBanner from "./PoliticalBanner";
+import EventGallery from "./PoliticalGallery";
+
+const EventPage = () => {
+  return (
+    <div 
+      className="min-h-screen flex flex-col bg-fixed bg-cover bg-center"
+    >
+      <Navbar />
+      
+      <main className="flex-grow">
+        <EventBanner />
+        <EventGallery />
+      </main>
+    </div>
+  );
+};
+
+export default EventPage;
---
+++ project-bolt-sb1-7ft7cjbm/frontend/src/components/Cinematography/Sports/SportsPage.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Navbar from "../../HomePage/Navbar";
+import EventBanner from "./SportsBanner";
+import EventGallery from "./SportsGallery";
+
+const EventPage = () => {
+  return (
+    <div 
+      className="min-h-screen flex flex-col bg-fixed bg-cover bg-center"
+    >
+      <Navbar />
+      
+      <main className="flex-grow">
+        <EventBanner />
+        <EventGallery />
+      </main>
+    </div>
+  );
+};
+
+export default EventPage;
